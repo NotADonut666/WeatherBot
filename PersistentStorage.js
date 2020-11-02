431b07382454f9009482865c3ecf3b7d0bf1394a
NotADonut666 <shimmerklaus@gmail.com>
2020-11-02 15:46:43 +0200

    APPID hide

--- PersistentStorage.js
+++ PersistentStorage.js
@@ -1,12 +1,14 @@
+const fs = require('fs');
 const storage = {
 AbsoluteNull: -273.15,
+
 requestURLS:{
     Ukraine:{
         Kharkiv: [
-          `https://api.openweathermap.org/data/2.5/forecast?id=706483&APPID=785e789096cf2276c2f7c8dc9ef9af64`
+          `https://api.openweathermap.org/data/2.5/forecast?id=706483&APPID=Your APPID here`
         ],
-        Kiev: [`https://api.openweathermap.org/data/2.5/forecast?id=703448&APPID=785e789096cf2276c2f7c8dc9ef9af64`],
-        Chuhuyiv: ['https://api.openweathermap.org/data/2.5/forecast?id=710374&APPID=785e789096cf2276c2f7c8dc9ef9af64']
+        Kiev: [`https://api.openweathermap.org/data/2.5/forecast?id=703448&APPID=Your APPID here`],
+        Chuhuyiv: ['https://api.openweathermap.org/data/2.5/forecast?id=710374&APPID=Your APPID here']
     }
     },
 Town:{
