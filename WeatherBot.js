e09dd089a97c7fd631714668e195fe8f96bfc413
NotADonut666 <shimmerklaus@gmail.com>
2021-01-11 13:51:48 +0200

    Update WeatherBot.js
    I'm refactored code in file "WeatherBot.js" into classes

--- WeatherBot.js
+++ WeatherBot.js
@@ -1,51 +1,54 @@
 const PersistentStorage = require('./PersistentStorage');
 const fetch = require("node-fetch");
 
-function WeatherBot(_TelegramBot)
+
+class WeatherBot
 {
-    this.TelegramBot = _TelegramBot;
+    constructor(_TelegramBot)
+    {
+        this.TelegramBot = _TelegramBot;
 
-    this.WeatherSourcesSites = {};
-
-    this.Initialize();
-}
-
-WeatherBot.prototype.Initialize = function() 
-{
-    this.WeatherSourcesSites = PersistentStorage.requestURLS.Ukraine;
+        this.WeatherSourcesSites = {};
     
-    this.TelegramBot.on("polling_error", (err) => console.log(err));
+        this.Initialize();
+    }
 
-    var This = this;
+    Initialize()
+    {
+        this.WeatherSourcesSites = PersistentStorage.requestURLS.Ukraine;
 
-    this.TelegramBot.onText(/^\/weather/,  function (msg) {
-        This.TelegramBot.sendMessage(msg.chat.id, "In what town do you want to see forecast?", PersistentStorage.Town);
-        This.TelegramBot.on('callback_query', query => 
-        {
-            This.ProcessSendWeather(query.data, msg);
-            This.TelegramBot.removeListener('callback_query');
+        this.TelegramBot.on("polling_error", (err) => console.log(err));
+
+        var This = this;
+    
+        this.TelegramBot.onText(/^\/weather/,  function (msg) {
+            This.TelegramBot.sendMessage(msg.chat.id, "In what town do you want to see forecast?", PersistentStorage.Town);
+            This.TelegramBot.on('callback_query', query => 
+            {
+                This.ProcessSendWeather(query.data, msg);
+                This.TelegramBot.removeListener('callback_query');
+            });
         });
-    });
-}
-
-WeatherBot.prototype.ProcessSendWeather = function(TownName, Msg) 
-{
-    var This = this;
-
-    fetch(this.WeatherSourcesSites[TownName]).then(function(resp)
-    { return resp.json();})
-    .then((data) => 
-    { 
-      let DataString = "";
-      for (let i = 0; i < Math.min(3, data.list.length); i++)
-      {
-        let WeatherData = data.list[i];
-        DataString +=
-        `🕐${WeatherData.dt_txt}:  ${(WeatherData.main.temp + PersistentStorage.AbsoluteNull).toFixed(2)+" °C\n"}`;
-      }
-
-      This.TelegramBot.sendMessage(Msg.chat.id, DataString);
-    });
+    }
+    
+    ProcessSendWeather(TownName, Msg)
+    {
+        var This = this;
+        fetch(this.WeatherSourcesSites[TownName]).then(function(resp)
+        { return resp.json();})
+        .then((data) => 
+        { 
+          let DataString = "";
+          for (let i = 0; i < Math.min(3, data.list.length); i++)
+          {
+            let WeatherData = data.list[i];
+            DataString +=
+            `🕐${WeatherData.dt_txt}:  ${(WeatherData.main.temp + PersistentStorage.AbsoluteNull).toFixed(2)+" °C\n"}`;
+          }
+            
+          This.TelegramBot.sendMessage(Msg.chat.id, DataString);
+        });
+    }
 }
 
 module.exports = WeatherBot;
