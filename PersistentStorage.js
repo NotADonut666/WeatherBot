ea309ac67c796420cc90b53aadf852d484fe11d0
NotADonut666 <73786233+NotADonut666@users.noreply.github.com>
2021-09-11 17:49:07 +0300

    Code style fix

--- PersistentStorage.js
+++ PersistentStorage.js
@@ -1,14 +1,15 @@
 const fs = require('fs');
+const APPID = "Your APPID here";
 const storage = {
 AbsoluteNull: -273.15,
 
 requestURLS:{
     Ukraine:{
         Kharkiv: [
-          `https://api.openweathermap.org/data/2.5/forecast?id=706483&APPID=Your APPID here`
+          `https://api.openweathermap.org/data/2.5/forecast?id=706483&APPID=${APPID}`
         ],
-        Kiev: [`https://api.openweathermap.org/data/2.5/forecast?id=703448&APPID=Your APPID here`],
-        Chuhuyiv: ['https://api.openweathermap.org/data/2.5/forecast?id=710374&APPID=Your APPID here']
+        Kiev: [`https://api.openweathermap.org/data/2.5/forecast?id=703448&APPID=${APPID}`],
+        Chuhuyiv: ['https://api.openweathermap.org/data/2.5/forecast?id=710374&APPID=${APPID}']
     }
     },
 Town:{
